cdcc60459ac6f361b1a26e02e61b4b9f6df55c72
nancel <nestor.ancel@gmail.com>
2025-02-04 18:11:11 -0300

    add filter by category and product details

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,13 +1,27 @@
+import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
+
 interface CategoryProps {
-  params: Promise<{
-    categories: string[]
-  }>,
-  searchParams?: Promise<string>
+  params: {
+    categories: string[],
+  }
+  searchParams?: string
 }
 
-export default async function Category(props: CategoryProps) {
+export default async function Category(props: CategoryProps){
   const { categories } = await props.params
-  return (
-    <h1>Category: {categories} </h1>
+  let products = []
+  const collections = await getCollections()
+  
+  if (categories?.length > 0) {
+    const selectedCollectionId = collections.find((collection) => collection.handle === categories[0]).id
+    products = await getCollectionProducts(selectedCollectionId)
+  }else {
+    products = await getProducts()
+  }
+
+  return(
+    <ProductsWrapper products={products}/>
   )
 }
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -3,5 +3,9 @@ import { env } from "app/config/env"
 export const shopifyUrls = {
   products: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`
+  },
+  collections: {
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/smart_collections.json`,
+    'products': (id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/collections/${id}/products.json`
   }
 }
