99c8422e9c53a13b4d9db1d9778f594c919a13b8
nancel <nestor.ancel@gmail.com>
2025-02-14 17:53:35 -0300

    cache, main products, metadata

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,7 +2,8 @@ import { env } from "app/config/env"
 
 export const shopifyUrls = {
   products: {
-    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`
+    'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`,
+    'mainProducts': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/collections/484912496958/products.json`,
   },
   collections: {
     'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2025-01/smart_collections.json`,
